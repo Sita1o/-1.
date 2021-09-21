194f2adce8833df04a56f871363ef898400ac947
Sita1o <90318279+Sita1o@users.noreply.github.com>
2021-09-21 18:39:27 +0300

    Add files via upload

---
+++ sitalo.lab1.cpp
@@ -0,0 +1,39 @@
+﻿//#include "stdafx.h"
+#include "iostream" 
+#include "cstdlib" 
+
+#include "iomanip" 
+
+using namespace std;
+
+int main()
+
+{
+
+	setlocale(LC_ALL, "Russian");
+
+	cout << "HELLO!!!\n";
+
+	cout << "\t\t -----\n";
+
+	cout << "\t\t|     |\n";
+
+	cout << "\t\t|     |\n";
+
+	cout << "\t\t -----\n";
+
+	cout << right << setw(16) << "Фамилия:" << "Ситало\n" ;
+	cout << right << setw(16) << "Имя:" << "Антон\n";
+	cout << right << setw(16) << "Отчество:" << "Владимировыч\n";
+	cout << right << setw(16) << "Пол:" << "Мужской\n";
+	cout << right << setw(16) << "Дата рождения:" << "30.01.2004\n";
+	cout << right << setw(16) << "Адрес:" << "Ветеренарная 12\n";
+	cout << right << setw(16) << "Номер телефона:" << "093 352 7942\n";
+	cout << right << setw(16) << "Место учебы:" << "ДНУ\n";
+	cout << right << setw(16) << "Класс:" << "ПА-21-1\n";
+	cout << right << setw(16) << "Хобби:" << "игры и рисование \n";
+	system("pause");
+
+	return 0;
+
+}
